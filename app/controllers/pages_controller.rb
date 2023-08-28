9653bec6193636eceaffffca725800b7547445a7
Erwin Gatica <erwin.gmanz@gmail.com>
2023-08-28 12:37:13 -0400

    Rails new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+  def about
+
+  end
+  def scott
+
+  end
+  def office
+
+  end
+  def animatronics
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "about", to: "pages#about";
+  get "scott", to: "pages#scott";
+  get "office", to: "pages#office";
+  get "animatronics", to: "pages#animatronics";
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
